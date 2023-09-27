3c7ff1bf0942d99164f397358dfa79ee8408096a
libakmatusko <libak.matusko@gmail.com>
2023-09-27 21:59:56 +0200

    na podstranky

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+var url = window.location.pathname;
+alert(url.substring(url.lastIndexOf('/')+1));
+
 let questionForm = document.getElementById("question_form");
 questionForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     let question = document.getElementById("question");
-    if (question.value != "IDK") {
+    if (question.value != "tu napíš svoju otázku") {
         // tu sa bude spracovavat request s API
         //question je vstup
         console.log(question.value)
         //answer buder vystup.
+        let answer = "Odpoved od chat GPT"
+        console.log(answer)
+        document.getElementById("answer").innerText = answer;
     }
-});
+})
